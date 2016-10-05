75ec881d5f25ab3653d9f1e266380a78d4ae10c7
Raphael Brule <raphael_brule@hotmail.com>
2016-10-05 10:19:39 -0400

    Interface

---
+++ AS2D_2016/AS2D_2016/IDestructible.cs
@@ -0,0 +1,13 @@
+﻿/* Author :            Raphael Brule
+   File :           IDestructible.cs
+   Date :              05 October 2016
+   Description :       This interface represents a destructible object.*/
+
+namespace AS2D_2016
+{
+    interface IDestructible
+    {
+        //To implement
+        bool ToDestroy { get; }
+    }
+}
