b78ab4b17d9063ca3684b9fd683ed7975ea13572
Raphael Brule <raphael_brule@hotmail.com>
2016-10-05 10:19:55 -0400

    Interface

---
+++ AS2D_2016/AS2D_2016/ICollidable.cs
@@ -0,0 +1,13 @@
+﻿/* Author :            Raphael Brule
+   File :           ICollidable.cs
+   Date :              05 October 2016
+   Description :       This interface represents a collidable object.*/
+
+namespace AS2D_2016
+{
+    interface ICollidable
+    {
+        //To implement
+        bool IsColliding(object otherObject);
+    }
+}
