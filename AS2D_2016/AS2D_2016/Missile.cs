fdb8127d44db0c90920f09b720d675f81077d65b
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2016-10-05 11:44:35 -0400

    Missile and Text

---
+++ AS2D_2016/AS2D_2016/Missile.cs
@@ -0,0 +1,80 @@
+/*
+Missile.cs
+----------
+
+By Matthew Godin
+
+Role : Component inheriting from AnimatedSprite
+       managing a missile accelerating
+       upwards and its explosion
+
+Created : 5 October 2016
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace AS2D_2016
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class Sphere //: AnimatedSprite
+    {
+        //Property initially managed by the constructor
+        float DisplacementUpdateInterval { get; set; }
+        string ImageNameMissile { get; set; }
+        Vector2 ImageDescriptionMissile { get; set; }
+        string ExplosionImageName { get; set; }
+        Vector2 ImageExplosionDescription { get; set; }
+
+        /// <summary>
+        /// Missile constructor
+        /// </summary>
+        /// <param name="game">Game object</param>
+        /// <param name="imageName">Image name (string)</param>
+        /// <param name="position">Position (Vector2)</param>
+        /// <param name="displayZonee">Display zone (Rectangle)</param>
+        /// <param name="imageDescription">Image description (Vector2)</param>
+        /// <param name="animationUpdateInterval">Animation update interval (float)</param>
+        /// <param name="displacementUpdateInterval">Displacement update interval (float)</param>
+        public Sphere(Game game, string imageNameMissile, Vector2 position, Rectangle displayZonee, Vector2 imageDescriptionMissile, string imageNameExplosion, Vector2 imageExplosionDescription, float animationUpdateInterval, float displacementUpdateInterval) : base(game, imageNameMissile, position, displayZonee, imageDescriptionMissile, animationUpdateInterval)
+        {
+            DisplacementUpdateInterval = displacementUpdateInterval;
+            ImageNameMissile = imageNameMissile;
+            ImageDescriptionMissile = imageDescriptionMissile;
+            ExplosionImageName = imageNameExplosion;
+            ImageExplosionDescription = imageExplosionDescription;
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+    }
+}
