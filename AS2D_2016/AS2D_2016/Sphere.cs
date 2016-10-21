488dbf9599c0b1350b4c1583daae6f6c2096cb71
201554258 <201554258@P106-13>
2016-10-21 10:13:00 -0400

    ok

--- AS2D_2016/AS2D_2016/Sphere.cs
+++ AS2D_2016/AS2D_2016/Sphere.cs
@@ -12,15 +12,7 @@ Created : 12 October 2016
 Modified : 15 October 2016
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace XNAProject
 {
